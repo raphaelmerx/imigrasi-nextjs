5c4b4372e42a7ac07cc33cabea6e78506e814ac3
Raphael Merx <raphael.merx@gmail.com>
2023-05-22 20:39:13 +0800

    Center navbar text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import king from "../public/king.png";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[60px] border-b border-gray-300 py-2 px-4 sm:px-8 items-center sm:justify-between justify-center">
+    <div className="flex h-[60px] border-b border-gray-300 py-2 px-4 sm:px-8 items-center justify-center">
       <div className="font-semibold sm:text-2l md:text-2xl flex items-center">
         <div className="ml-2">
             AI-powered Q&A on Imigrasi.go.id

fabe2f3860a8d93ec0442ae80168d3bc06cf307e
Alain Scialoja <>
2023-05-23 16:14:18 +0800

    Add basic voting UI

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Backend is at https://github.com/raphaelmerx/imigrasi-bot
 
 Setup:
 * Install deps: `npm install`
-* Create a `.env` file with a `NEXT_PUBLIC_BACKEND_URL` variable (prefix the url with `ws://` instead of `http://`)
+* Create a `.env` file with the backend variables
+```
+NEXT_PUBLIC_WS_BACKEND_URL="ws://127.0.0.1:9000"
+NEXT_PUBLIC_BACKEND_URL="http://127.0.0.1:9000"
+```
 * For development: `npm run dev`
 * To deploy, e.g. using Vercel: `npm run build`
--- components/Answer/Answer.tsx
+++ components/Answer/Answer.tsx
@@ -1,18 +1,80 @@
-import React, { useEffect, useState } from "react";
-import styles from "./answer.module.css";
+import React, { useEffect, useState } from 'react';
+import { IconThumbUp, IconThumbDown } from '@tabler/icons-react';
 
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 interface AnswerProps {
   text: string;
 }
 
 export const Answer: React.FC<AnswerProps> = ({ text }) => {
-  const paragraphs = text.split('\n').map((paragraph, index) => (
-    <p key={index}>{paragraph}</p>
-  ));
+  const paragraphs = text
+    .split('\n')
+    .map((paragraph, index) => <p key={index}>{paragraph}</p>);
+  // const inputRef = useRef<HTMLInputElement>(null);
+
+  const [query, setQuery] = useState<string>('');
+  const [chunks, setChunks] = useState<DocumentChunk[]>([]);
+  const [answer, setAnswer] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [showExamples, setShowExamples] = useState<boolean>(true);
+
+  const [matchCount, setMatchCount] = useState<number>(5);
+
+  const handleVote = async (vote: number) => {
+    if (!vote) {
+      alert('Please enter a query.');
+      return;
+    }
+    console.log('Loading:', loading);
+    if (loading) {
+      return;
+    }
+
+    if (vote > 0) {
+      console.log('Thumbs-up');
+    } else if (vote < 0) {
+      console.log('Thumbs-down');
+    }
+
+    // Send the vote value via POST
+    const response = await fetch(`${BACKEND_URL}/vote`, {
+      method: 'POST',
+      body: JSON.stringify({vote: vote}),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+
+    if (!response.ok) {
+      /* Handle */
+      console.log("Voting error")
+    }else {
+
+      console.log("Voting success")
+    }
+
+  };
 
   return (
     <div>
-       {paragraphs}
+      {paragraphs}
+      <div className="flex align-text-bottom border-t border-gray-300 my-2 justify-end">
+        <p className="pr-2 pt-1">Was this answer useful?</p>
+        <button>
+          <IconThumbUp
+            onClick={() => handleVote(1)}
+            className="hover:cursor-pointer hover:text-blue-600 sm:h-6"
+          />
+        </button>
+        <button>
+          <IconThumbDown
+            onClick={() => handleVote(-1)}
+            className="hover:cursor-pointer hover:text-blue-600 sm:h-6"
+          />
+        </button>
+      </div>
     </div>
   );
 };

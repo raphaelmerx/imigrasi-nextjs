53d9373cd67845c6080b53694c8d97105be32330
Alain Scialoja <>
2023-05-24 21:03:42 +0800

    Fix vercel complaining about supabase

--- utils/index.ts
+++ utils/index.ts
@@ -1,50 +1,60 @@
-import { OpenAIModel } from "@/types";
-import { createClient } from "@supabase/supabase-js";
-import { createParser, ParsedEvent, ReconnectInterval } from "eventsource-parser";
+import { OpenAIModel } from '@/types';
+import { createClient } from '@supabase/supabase-js';
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from 'eventsource-parser';
 
 import { v4 as uuidv4 } from 'uuid';
 
-// export const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost';
+const subabase_key = process.env.SUPABASE_SERVICE_ROLE_KEY || 'key';
+export const supabaseAdmin = createClient(
+  supabase_url,
+  subabase_key
+);
 
 export const OpenAIStream = async (prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const res = await fetch('https://api.openai.com/v1/chat/completions', {
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
     },
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       model: OpenAIModel.DAVINCI_TURBO,
       messages: [
         {
-          role: "system",
-          content: "You are a helpful assistant that accurately answers queries using Wait But Why posts. Use the text provided to form your answer, but avoid copying word-for-word from the posts. Try to use your own words when possible. Keep your answer under 5 sentences. Be accurate, helpful, concise, and clear."
+          role: 'system',
+          content:
+            'You are a helpful assistant that accurately answers queries using Wait But Why posts. Use the text provided to form your answer, but avoid copying word-for-word from the posts. Try to use your own words when possible. Keep your answer under 5 sentences. Be accurate, helpful, concise, and clear.',
         },
         {
-          role: "user",
-          content: prompt
-        }
+          role: 'user',
+          content: prompt,
+        },
       ],
       max_tokens: 150,
       temperature: 0.0,
-      stream: true
-    })
+      stream: true,
+    }),
   });
 
   if (res.status !== 200) {
-    throw new Error("OpenAI API returned an error");
+    throw new Error('OpenAI API returned an error');
   }
 
   const stream = new ReadableStream({
     async start(controller) {
       const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === "event") {
+        if (event.type === 'event') {
           const data = event.data;
 
-          if (data === "[DONE]") {
+          if (data === '[DONE]') {
             controller.close();
             return;
           }
@@ -65,16 +75,15 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
       for await (const chunk of res.body as any) {
         parser.feed(decoder.decode(chunk));
       }
-    }
+    },
   });
 
   return stream;
 };
 
 export const getSessionId = () => {
-    
-    if (localStorage.getItem('session-id') === null) {
-      localStorage.setItem('session-id', uuidv4());
-    }
-    return localStorage.getItem('session-id');
-}
+  if (localStorage.getItem('session-id') === null) {
+    localStorage.setItem('session-id', uuidv4());
+  }
+  return localStorage.getItem('session-id');
+};
